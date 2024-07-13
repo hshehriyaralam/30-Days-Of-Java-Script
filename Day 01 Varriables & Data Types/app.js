1480f906094320535eaec5fb07645ff59abb3768
hshehriyaralam <hshehriyaralam@gmail.com>
2024-07-13 15:29:48 -0600

    Day 01

---
+++ Day 01 Varriables & Data Types/app.js
@@ -0,0 +1,78 @@
+// Activity 1 : Variable Declarations
+var UserAge = 21;
+console.log(UserAge);
+
+let userName = "Shahmeer";
+console.log(userName);
+
+// Activity 2 : Constant Declarations
+const userIsPresen = true
+console.log(userIsPresen);
+
+// Activity 3 : Data Types
+let studentName = "Shahmeer";
+let studentdAge = 21;
+let  studentIsPresent = true;
+let studentSubjects = ["Programing Fundamentals", "DSA", "AI"];
+let studentShedule = {
+    Monday: "Programing Fundamentals",
+    Tuesday: "DSA",
+    Wednesday : "AI"
+};
+
+console.log(typeof studentName);
+console.log(typeof studentdAge);
+console.log(typeof studentIsPresent);
+console.log(typeof studentSubjects);
+console.log(typeof studentShedule);
+
+
+// Activity 4 : Reassign Variable
+let FirstName = "Shehriyar ";
+console.log(FirstName);
+FirstName = "Shehriyar ALam"
+console.log(FirstName);
+
+// Activity 5 : Understanding const
+const lastName = "Sherry"
+console.log(lastName);
+lastName = "Sherry KHan"
+console.log(lastName);
+
+/*
+ Feature Request 
+ 01) * Variables Types Console Log*
+ When I declare all Data Types in variables and console with "typeof" get the result is 
+ strings Data type is Strings 
+ Number Data type is Numbers
+ Boolean Data type is Boolean 
+ But Array Data Type is Object
+ and Object Data Type is Object.
+
+
+
+02) *Reassigment Demo*
+When I Decalre variables using let , const and var
+Shocking things is var Allow to Reassign and Reinitialize
+But Let only allow Reassign 
+and const are don't allow to Reassign and Reinitialize
+const with the time of Reinitialize show Error in Code Editior
+But with the time of Reassign don't show error in Code Editor But When I run code browser and Node Enviroment Show the Error 
+
+
+
+
+********************************* Achievments *****************************
+01) know how to declare variables using var, let and const
+02) Understand the different data Types in Java Script
+03) Be able to use the typeof Operators to identify the data types of a variable
+04) Understand the concept of variable reassinment and the immutability of const variables
+
+
+
+
+
+
+
+
+*/
